d35e19855259483786696e3c48bf483e4c6d96ae
Mihika7 <94743689+Mihika7@users.noreply.github.com>
2021-12-28 14:18:05 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -46,7 +46,7 @@ class Bot(Client):
             sys.exit()
 
         self.set_parse_mode("html")
-        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by 𝘾𝙤𝙙𝙚 𝕏 𝘽𝙤𝙩𝙯\nhttps://t.me/all_web_series_mihika")
+        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by All web series\nhttps://t.me/all_web_series_mihika")
         self.username = usr_bot_me.username
 
     async def stop(self, *args):
